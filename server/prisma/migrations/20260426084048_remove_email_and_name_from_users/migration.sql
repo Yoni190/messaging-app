e0e4649d63c3c97279d66881245846aa3380e2d7
Yoni190 <yfisuhu@gmail.com>
2026-04-26 11:46:15 +0300

    Remove email and name from users table

---
+++ server/prisma/migrations/20260426084048_remove_email_and_name_from_users/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `f_name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `l_name` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "email",
+DROP COLUMN "f_name",
+DROP COLUMN "l_name";

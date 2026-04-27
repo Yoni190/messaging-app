e9f8943efc7cb158a45576b5cc5a0d41f838af68
Yoni190 <yfisuhu@gmail.com>
2026-04-27 17:19:10 +0300

    Create login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    console.log(formData)
+  }
+  
   return (
     <div>
-        <h1 className='text-red-800 text-4xl'>Login</h1>
+        <form onSubmit={handleLogin}>
+          <h1 className='text-red-800 text-4xl'>Login</h1>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder='John Doe'
+            className='border p-1'
+            value={formData.username}
+            onChange={(e) => setFormData({...formData, username: e.target.value})}/>
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder='********'
+            className='border p-1' 
+            value={formData.password}
+            onChange={(e) => setFormData({...formData, password: e.target.value})}/>
+
+            <button className='border p-1'>
+              Login
+            </button>
+        </form>
     </div>
   )
 }

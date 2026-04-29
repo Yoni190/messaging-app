cf299e882076ca9d864ee4c40fac59a9ea185378
Yoni190 <yfisuhu@gmail.com>
2026-04-29 11:05:57 +0300

    Display users in the sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Sidebar = () => {
+    const [users, setusers] = useState([])
+    const API_URL=import.meta.env.VITE_API_URL
+
+    useEffect(() => {
+      const getUsers = async() => {
+        const token = localStorage.getItem('token')
+        try {
+            const res = await fetch(`${API_URL}/users`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+
+            const data = await res.json()
+            setusers(data.users)
+        } catch (error) {
+            console.error(error)
+        }
+      }
+
+      getUsers()
+    }, [])
+    
   return (
-    <div className='border p-4 w-64 bg-gray-100'>
-        <h2 className='text-center'>No Messages Yet</h2>
+    <div className='border w-64 bg-gray-100'>
+
+        {users.map((user) => (
+            <div key={user.id} className='border p-4'>
+                <h2>{user.username}</h2>
+            </div>
+        ))}
     </div>
   )
 }

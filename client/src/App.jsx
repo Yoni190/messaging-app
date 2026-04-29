e5795c09fdef66e8373ffce125639aacc14cda85
Yoni190 <yfisuhu@gmail.com>
2026-04-29 10:57:17 +0300

    Add sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,34 +4,38 @@ import Register from './pages/Register'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
-
-
-
-
+function AppLayout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex">
+        <Sidebar />
+        <div className="flex-grow">
+          <Home />
+        </div>
+      </main>
+    </div>
+  )
+}
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div className="min-h-screen flex flex-col">
-        <Header />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
 
-        <main className="flex-grow">
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route
-              path='/home'
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-              />
-          </Routes>
-        </main>
-    </div>
+        <Route
+          path='/home'
+          element={
+            <ProtectedRoute>
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='border p-4 w-64 bg-gray-100'>
+        <h2 className='text-center'>No Messages Yet</h2>
+    </div>
+  )
+}
+
+export default Sidebar

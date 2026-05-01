f5a4eb98d1b1c207d27e696cdf302248561c86c3
Yoni190 <yfisuhu@gmail.com>
2026-05-01 11:14:40 +0300

    Add logout button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
+import { useAuth } from '../../hooks/useAuth'
 
 const Header = () => {
+  const navigate = useNavigate()
+
+  const { logout } = useAuth()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/')
+  }
   return (
-    <header className='border p-4'>
+    <header className='border p-4 flex justify-between'>
         <Link to={'/home'} className='text-2xl'>Messaging App</Link>
+
+        <button className='border p-2 rounded bg-red-500 text-white cursor-pointer' onClick={handleLogout}>Log Out</button>
     </header>
   )
 }
